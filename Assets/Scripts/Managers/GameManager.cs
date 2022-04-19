c8d403262e12930c2306e15d73208546c86ee6d7
Da2ny <gabriel.danjon@epitech.eu>
2022-04-19 20:28:47 +0900

    [ADD]: Coin goal validation

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -5,8 +5,11 @@ public class GameManager : StaticInstance<GameManager> {
     public GameObject coinList;
 
     public bool isLevelCompleted() {
-        if (coinList && coinList.transform.childCount != 0)
+        if (coinList && coinList.transform.childCount != 0) {
+            print("Non validation");
             return false;
+        }
+        print("Validation");
         return true;
     } 
 }
