e6d5c8892c8e842e81d62230488f5bc13863befc
Alkeon <lucas.arsene@epitech.eu>
2022-05-18 01:16:05 +0900

    almost finished level

--- Assets/_Scripts/PressurePlate/PressurePlateTrigger.cs
+++ Assets/_Scripts/PressurePlate/PressurePlateTrigger.cs
@@ -7,6 +7,8 @@ public class PressurePlateTrigger : MonoBehaviour
     MeshCollider _plateCollider;
     public GameObject _tile;
     public float speed = 2f;
+    public float tileSpeed = 2f;
+    public float plateformSpeed = 2f;
     public bool pressed = false;
 
     void Start()
@@ -18,20 +20,34 @@ public class PressurePlateTrigger : MonoBehaviour
     void Update()
     {
         if (pressed) {
-            MoveTileDown();
+            if (_tile.CompareTag("UpAndDown")) {
+                MoveTileUpAndDown();
+            }
+            else if (_tile.CompareTag("Spikes") || _tile.CompareTag("Tile")) {
+                MoveTileDown();
+            }
+            else if (_tile.CompareTag("Platform")) {
+                MovePlateform();
+            }
         }
         else {
-            MoveTileUp();
+            if (_tile.CompareTag("Tile") || _tile.CompareTag("UpAndDown")) {
+                MoveTileUp();
+            }
         }
     }
 
     void OnTriggerEnter(Collider col) {
-        Debug.Log("Tamer");
-        pressed = true;
+        if (!_tile.CompareTag("Spikes") && !col.CompareTag("Player")) {
+            pressed = true;
+        }
+        if (_tile.CompareTag("Spikes") && col.CompareTag("Player")) {
+            pressed = true;
+            _tile.GetComponent<BoxCollider>().enabled = false;
+        }
     }
 
     void OnTriggerExit(Collider col) {
-        Debug.Log("Down");
         pressed = false;
     }
 
@@ -45,5 +61,19 @@ public class PressurePlateTrigger : MonoBehaviour
         if (_tile.transform.position.y < 3.23) {
             _tile.transform.Translate(Vector3.up * Time.deltaTime * speed);
         }
+    }
+
+    private void MovePlateform() {
+        if (_tile.transform.position.x > -2.062999 || _tile.transform.position.x < -6.18) {
+            plateformSpeed = plateformSpeed * -1;
+        }
+        _tile.transform.Translate(Vector3.right * Time.deltaTime * plateformSpeed);
+    }
+
+    private void MoveTileUpAndDown() {
+        if (_tile.transform.position.y < 1.125752 || _tile.transform.position.y > 3.23) {
+            tileSpeed = tileSpeed * -1;
+        }
+        _tile.transform.Translate(Vector3.up * Time.deltaTime * tileSpeed);
     }
 }
